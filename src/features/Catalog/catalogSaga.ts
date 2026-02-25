8de4c9312dd065706e66240df9ea68b01c82550e
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2026-02-25 21:53:07 +0500

    feat(catalog): Написали сагу каталога

--- src/features/Catalog/catalogSaga.ts
+++ src/features/Catalog/catalogSaga.ts
@@ -1,21 +1,51 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
+import { call, put, select, takeLatest } from 'redux-saga/effects'
 import {
   catalogFailed,
+  catalogLoadMoreRequested,
+  catalogLoadMoreSucceeded,
   catalogRequested,
   catalogSucceeded,
 } from './catalogSlice'
 import { getItems } from '../../domain/catalog/api'
 import { getErrorMessage } from '../../domain/common/errors'
+import type { RootState } from '../../app/store'
 
-function* handleCatalog(): Generator {
+function* handleCatalogRequested(): Generator {
   try {
-    const data = yield call(getItems)
+    const categoryId = yield select(
+      (state: RootState) => state.categories.activeCategoryId,
+    )
+
+    const data = yield call(getItems, {
+      categoryId: categoryId as number,
+      offset: 0,
+    })
     yield put(catalogSucceeded(data))
   } catch (error) {
     yield put(catalogFailed(getErrorMessage(error)))
   }
 }
 
+function* handleLoadMoreRequested(): Generator {
+  try {
+    const categoryId = yield select(
+      (state: RootState) => state.categories.activeCategoryId,
+    )
+
+    const offset = yield select((state: RootState) => state.catalog.offset)
+
+    const data = yield call(getItems, {
+      categoryId: categoryId as number,
+      offset: offset as number,
+    })
+
+    yield put(catalogLoadMoreSucceeded(data))
+  } catch (error) {
+    yield put(catalogFailed(getErrorMessage(error)))
+  }
+}
+
 export function* watchCatalog() {
-  yield takeLatest(catalogRequested.type, handleCatalog)
+  yield takeLatest(catalogRequested.type, handleCatalogRequested)
+  yield takeLatest(catalogLoadMoreRequested.type, handleLoadMoreRequested)
 }
