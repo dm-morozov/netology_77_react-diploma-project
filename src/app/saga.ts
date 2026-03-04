dfd1869ec39dd791c8ae4f4a8af0f85d81392f42
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2026-03-04 15:03:39 +0500

    fix(store, saga): подключили, saga и action productDetails

--- src/app/saga.ts
+++ src/app/saga.ts
@@ -2,6 +2,7 @@ import { all } from 'redux-saga/effects'
 import { watchTopSales } from '../features/TopSales/topSalesSaga'
 import { watchCategories } from '../features/Categories/categoriesSaga'
 import { watchCatalog } from '../features/Catalog/catalogSaga'
+import { watchProductDetails } from '../features/ProductDetails/productDetailsSaga'
 
 export function* rootSaga() {
   yield all([
